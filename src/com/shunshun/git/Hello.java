6f16842178fd88b29b1d2e709fd544f3663651f4
sunzhongshun <zhongshun_sun@126.com>
2019-10-30 15:27:03 +0800

    first commit

---
+++ src/com/shunshun/git/Hello.java
@@ -0,0 +1,13 @@
+package com.shunshun.git;
+
+/**
+ * @author 孙忠顺
+ * @create 2019-10-30 15:23
+ */
+public class Hello {
+
+	public static void main(String[] args){
+		System.out.println("hello world");
+	}
+
+}
